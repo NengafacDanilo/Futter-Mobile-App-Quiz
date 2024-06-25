c47e797de31f36dd792b0fa104ffe546d3930a4c
NengafacDanilo <157296973+NengafacDanilo@users.noreply.github.com>
2024-06-25 20:17:15 +0100

    Add files via upload

---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,24 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "c:\\dev\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "C:\\dev\\project\\flutter-quiz-app" PROJECT_DIR)
+
+set(FLUTTER_VERSION "0.1.0" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 0 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=c:\\dev\\flutter"
+  "PROJECT_DIR=C:\\dev\\project\\flutter-quiz-app"
+  "FLUTTER_ROOT=c:\\dev\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=C:\\dev\\project\\flutter-quiz-app\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=C:\\dev\\project\\flutter-quiz-app"
+  "FLUTTER_TARGET=C:\\dev\\project\\flutter-quiz-app\\lib\\screens\\dashboard.dart"
+  "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ==,RkxVVFRFUl9XRUJfQ0FOVkFTS0lUX1VSTD1odHRwczovL3d3dy5nc3RhdGljLmNvbS9mbHV0dGVyLWNhbnZhc2tpdC9lNzZjOTU2NDk4ODQxZTFhYjQ1ODU3N2QzODkyMDAzZTU1M2U0ZjNjLw=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=C:\\dev\\project\\flutter-quiz-app\\.dart_tool\\package_config.json"
+)
